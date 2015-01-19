8b019645bc2cc6e7322a24ecd6bb39a81bd3d834
Steven Clontz <steven.clontz@gmail.com>
2015-01-19 13:14:36 -0500

    initial yeoman build

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularMathCentersApp
+ * @description
+ * # angularMathCentersApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularMathCentersApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularMathCentersApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularMathCentersApp
+ */
+angular.module('angularMathCentersApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
